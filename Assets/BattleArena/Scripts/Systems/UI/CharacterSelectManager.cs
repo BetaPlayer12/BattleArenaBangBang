e3a607e10897d31a6bec91111662a60c70f3d7dc
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2023-12-08 17:41:10 +0800

    Updated Charcater Select Button UX

--- Assets/BattleArena/Scripts/Systems/UI/CharacterSelectManager.cs
+++ Assets/BattleArena/Scripts/Systems/UI/CharacterSelectManager.cs
@@ -15,18 +15,33 @@ namespace BattleArena.Gameplay.Menu
         private CharacterSelectHandle m_player2;
         [SerializeField]
         private Button m_startCombatButton;
+        [SerializeField]
+        private Image m_startCombatButtonBackground;
+        [SerializeField]
+        private Color m_notReadyForCombatColor;
+        [SerializeField]
+        private Color m_readyForCombatColor;
 
         public InitializationCommand CreateCharacterInitializationCommand() => new InitializationCommand(m_player1.GetSelectedCharacter(), m_player2.GetSelectedCharacter());
 
         private void OnPlayerReadinessChange()
         {
             m_startCombatButton.interactable = m_player1.isReady && m_player2.isReady;
+            if (m_startCombatButton.interactable)
+            {
+                m_startCombatButtonBackground.color = m_readyForCombatColor;
+            }
+            else
+            {
+                m_startCombatButtonBackground.color = m_notReadyForCombatColor;
+            }
         }
 
         public void Reset()
         {
             m_player1.Reset();
             m_player2.Reset();
+            m_startCombatButtonBackground.color = m_notReadyForCombatColor;
         }
 
         private void Awake()
