2619b7debb06174d874e3dbc956f757cb21f4e57
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2023-12-09 17:00:32 +0800

    Smoke UI effect stuff

---
+++ Assets/BattleArena/Objects/Core/UI/SmokeUIEffect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeUIEffect : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform m_startPosition;
+    [SerializeField]
+    private RectTransform m_endPosition;
+    [SerializeField]
+    private float m_startDelay;
+
+    private void Update()
+    {
+        
+    }
+}
